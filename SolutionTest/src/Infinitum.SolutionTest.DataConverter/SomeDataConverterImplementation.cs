6a306a18896ebd16adc10768d79d28435fbb98f8
spek <spek@nxt.ru>
2020-09-30 19:49:49 +0300

    upd

---
+++ SolutionTest/src/Infinitum.SolutionTest.DataConverter/SomeDataConverterImplementation.cs
@@ -0,0 +1,15 @@
+﻿using Infinitum.SolutionTest.DataConverter.Abstractions;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infinitum.SolutionTest.DataConverter
+{
+    public class SomeDataConverterImplementation : ISomeDataConverter
+    {
+        public Task<SomeConvertedData> Convert(string data, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SolutionTest/src/Infinitum.SolutionTest.Host/Startup.cs
+++ SolutionTest/src/Infinitum.SolutionTest.Host/Startup.cs
@@ -1,4 +1,16 @@
+using Infinitum.SolutionTest.Application.CommandHandlers;
+using Infinitum.SolutionTest.DataConverter;
+using Infinitum.SolutionTest.DataConverter.Abstractions;
+using Infinitum.SolutionTest.Domain.DataModel;
+using Infinitum.SolutionTest.EntityFrameworkCore;
+using Infinitum.SolutionTest.EntityFrameworkCore.Repositories;
+using Infinitum.SolutionTest.Host.BackroundServices;
+using Infinitum.SolutionTest.Host.EntityFrameworkCore;
+using Infinitum.SolutionTest.Host.EntityFrameworkCore.Repositories;
+using Infinitum.SolutionTest.Host.Interfaces;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,11 +29,21 @@ namespace Infinitum.SolutionTest.Host
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMediatR(typeof(HandleSomeDataCommandHandler));
 
+            services.AddDbContext<TestHostDbContext>(opt =>
+                opt.UseSqlServer(_configuration.GetConnectionString("TestDb")));
 
+            services.AddDbContext<TestDbContext>(opt =>
+                opt.UseSqlServer(_configuration.GetConnectionString("TestDb")));
 
+            services.AddTransient<ISomeDataRepository, SomeDataRepository>();
+            services.AddTransient<ISomeDataHostRepository, SomeDataHostRepository>();
 
-            
+            services.AddHostedService<DataDbScheduler>();
+
+            services.AddTransient<ISomeDataConverter, SomeDataConverterImplementation>();
+
         }
 
         public void Configure(IApplicationBuilder app, IHostApplicationLifetime lifetime, ILogger<Startup> logger)
